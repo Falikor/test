5803144972ff65885a22a5167e8df3f06a4d52b7
Victor <bugorv90@mail.ru>
2021-06-30 22:27:47 +0300

    wad

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 17760021 on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
